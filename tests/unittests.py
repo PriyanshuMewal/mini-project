a376a3ffbfba9d4ae1adafc50d6ef2bbf4338c1f
Prince Mewal <mewalpriyanshu0@gmail.com>
2025-12-29 12:13:42 +0530

    add In-Out Varification

--- tests/unittests.py
+++ tests/unittests.py
@@ -1,6 +1,11 @@
 import unittest
 import mlflow
 import os
+import pickle
+
+import pandas as pd
+
+from Fastapi.preprocessing import normalize_text
 
 class TestModelLoading(unittest.TestCase):
 
@@ -24,9 +29,36 @@ class TestModelLoading(unittest.TestCase):
         cls.alias = "champion"
         cls.model = mlflow.pyfunc.load_model(model_uri=f"models:/{cls.model_name}@{cls.alias}")
 
+        #     load vectorizer:
+        with open("models/vectorizer.pkl", "rb") as file:
+            cls.vectorizer = pickle.load(file)
+
 
     def test_model_loaded_properly(self):
          self.assertIsNotNone(self.model)
+
+
+    def test_model_signature(self):
+
+        test_point = "Layin n bed with a headache  ughhhh...waitin on your call..."
+
+        # preprocess text:
+        test_matrix = normalize_text(test_point)
+
+        # feature engineering over text:
+        test_df = self.vectorizer.transform([test_matrix])
+        test_df = pd.DataFrame(test_df.toarry(),
+                                  columns=self.vectorizer.get_feature_names_out())
+
+        prediction = self.model.predict(test_df)
+
+        # Varify the input shape:
+        self.assertEqual(test_df.shape[1],
+                         len(self.vectorizer.get_feature_names_out()))
+
+        # Varify the output shape:
+        self.assertEqual(len(prediction), test_df.shape[0])
+        self.assertEqual(len(prediction.shape), 1)
 
 if __name__ == "__main__":
     unittest.main()
